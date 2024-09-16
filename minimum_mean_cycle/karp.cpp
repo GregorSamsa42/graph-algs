758383331e5e2e6483cfdc5130febdf0d10652a7
GregorSamsa42 <georgi@georgi-kocharyan.de>
2024-09-16 21:52:19 +0200

    implemented Karp's alg

--- minimum_mean_cycle/karp.cpp
+++ minimum_mean_cycle/karp.cpp
@@ -1,5 +1,6 @@
 // Implementation of Karp's algorithm (1978) for finding a minimum mean cycle.
 // Author: Georgi Kocharyan
+
 #include <algorithm>
 #include <iostream>
 #include <limits>
@@ -9,28 +10,41 @@
 #include "digraph.h"
 #include "Kosaraju/kosaraju.h"
 
-double karp(const WeightedDigraph & G)
+double karp(const WeightedDigraph & G, int starting_node)
 {
     std::vector<std::vector<double>> F(G.num_nodes()+1, std::vector<double>(G.num_nodes(), std::numeric_limits<double>::max()));
+
+    // compute the values of F recursively. F[k][i] is interpreted as the length of the shortest path from starting_node to i containing exactly k edges.
+    F[0][starting_node] = 0;
+    for (int k = 1; k <= G.num_nodes(); ++k) {
+        for (int node_id = 0; node_id < G.num_nodes(); ++node_id) {
+            for (const auto & edge : G.adjList(node_id)) {
+                F[k][edge.to] = std::min(F[k][edge.to], F[k-1][edge.from] + edge.weight);
+            }
+        }
+    }
+
+    // compute min_{ x \in V(G) } max_{0 \leq k \leq n-1} (F[n][x] - F[k][x])/(n-k)
+    std::vector<double> mins(G.num_nodes());
+    for (int node_id = 0; node_id < G.num_nodes(); node_id++) {
+        std::vector<double> maxs(G.num_nodes());
+        for (int k = 0; k < G.num_nodes(); ++k) {
+            maxs[k] = (F[G.num_nodes()][node_id] - F[k][node_id])/(G.num_nodes() - k);
+        }
+        mins[node_id]  = *max_element(maxs.begin(), maxs.end());
+    }
+    // double* ptr = &(*min_element(mins.begin(), mins.end()));
+    // todo: store predecessor and output cycle
+    return *min_element(mins.begin(), mins.end());
 }
 
 int main()
 {
-    constexpr int size = 8;
+    constexpr int size = 3;
     WeightedDigraph G(size);
-    G.add_edge(3,4,2);
-    G.add_edge(4,3,3);
-    G.add_edge(5,6,6);
-    G.add_edge(6,7,1);
+    G.add_edge(0,1,2);
     G.add_edge(1,2,3);
-    G.add_edge(2,3,8);
-    G.add_edge(7,5,0.2);
-    G.add_edge(7,3,-1);
-    G.add_edge(0,3,1);
-    G.add_edge(3,0,5);
-    G.add_edge(4,6,3);
-    G.add_edge(0,7,0.5);
-    G.add_edge(4,2,1);
+    G.add_edge(2,0,6);
 
     // first check if G is strongly connected, convert it to a regular digraph.
 
@@ -38,13 +52,16 @@ int main()
     if (components != 1) {
         std::cout << "The given graph is not strongly connected." << std::endl;
     }
+    else {
+        double min_mean_cycle = karp(G, 0);
 
-    double min_mean_cycle = karp(G);
+        // output the distances
 
-    // output the distances
+        std::cout << "The minimum mean cycle has mean weight " << min_mean_cycle << "." << std::endl;
+        return 0;
+    }
 
-    std::cout << "The minimum mean cycle has mean weight " << min_mean_cycle << "." << std::endl;
 
-    return 0;
+    return 1;
 
 }
