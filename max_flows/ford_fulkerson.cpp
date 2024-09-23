6c108b090cf21bed7acba14a517526fcc6015beb
GregorSamsa42 <georgi@georgi-kocharyan.de>
2024-09-24 00:14:26 +0200

    ford fulkerson works

--- max_flows/ford_fulkerson.cpp
+++ max_flows/ford_fulkerson.cpp
@@ -13,20 +13,23 @@ bool rest_capacity_order(const NetworkEdge *a, const NetworkEdge *b)
     return (*a) < (*b);
 }
 
-void dfs(Network & G, const NetworkEdge & edge, const int & sink, std::vector<bool> & vis, std::vector<NetworkEdge> & path, bool & found) {
+void dfs(Network & G, NetworkEdge & edge, const int & sink, std::vector<bool> & vis, std::vector<NetworkEdge*> & path, bool & found) {
     if (vis[edge.to] || found || !edge.marking)
     {
         return;  //if node is already visited don't
     }
     vis[edge.to] = true;
-    path.push_back(edge);
+    path.push_back(&edge);
     if (edge.to == sink) {
         found = true;
         return;
     }
-    for(const auto & i: G.adjList(edge.to))
+    for(auto & i: G.adjList(edge.to))
     {
         dfs(G, i, sink, vis,path, found);
+        if (found) {
+            return;
+        }
     }
     path.pop_back();
 }
@@ -45,12 +48,12 @@ double ford_fulkerson(Network & G)
     }
     G_edges.sort(rest_capacity_order);
     // recursively add into residual graph, and stop as soon as there is an s-t path
-    std::vector<NetworkEdge> path;
-    path.reserve(G.num_nodes());
     bool found_path = true;
 
     // found_path is true for the while loop to start. after that it will only be set to false at the end if none is found
     while (found_path) {
+        std::vector<NetworkEdge*> path;
+        path.reserve(G.num_nodes());
         found_path = false;
         std::vector<bool> vis(G.num_nodes(), false);
         for (const auto & edge: G_edges) {
@@ -68,11 +71,12 @@ double ford_fulkerson(Network & G)
         // if a path has been found, augment appropriately
         if (found_path) {
             // find the edge in the path with maximal rest capacity
-            const double augment = (*std::max_element(path.begin(), path.end())).rest_capacity();
-            for (auto & edge: path) {
-                edge.pump(augment);
-                max_flow += augment;
+            // ignore the first edge as it is always fake_edge
+            const double augment = (*std::max_element(++path.begin(), path.end(),rest_capacity_order))->rest_capacity();
+            for (int i = 1; i < path.size(); i++) {
+                path[i]->pump(augment);
             }
+            max_flow += augment;
         }
     }
 return max_flow;
@@ -89,5 +93,10 @@ int main()
 
     const double max_flow = ford_fulkerson(G);
 
-    std::cout << max_flow << std::endl;
+    std::cout << "The maximum flow is " << max_flow << "." << std::endl;
+    for (int node_id = 0; node_id < size; ++node_id) {
+        for (const auto & edge: G.adjList(node_id)) {
+            std::cout << "The edge from " << edge.from << " to " << edge.to << " has capacity " << edge.weight << " and flow " << edge.flow << "." << std::endl;
+        }
+    }
 }
